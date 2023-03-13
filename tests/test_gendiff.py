982aee95c54b854f60bce5efd08639ff66d6d7ef
ggByron <shtyrbu.ya@gmail.com>
2023-03-13 12:25:36 +0500

    trying to fix errors

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -22,20 +22,27 @@ CORRECT_BASIC_DICT = {
 }
 
 
-@pytest.mark.parametrize("file_path, format, expected", [(JSON1, '.json', CORRECT_BASIC_DICT), (YAML1,'.yaml' or '.yml', CORRECT_BASIC_DICT)])
+@pytest.mark.parametrize(    
+    "file_path, format, expected", [
+    (JSON1, '.json', CORRECT_BASIC_DICT), 
+    (YAML1, '.yaml' or '.yml', CORRECT_BASIC_DICT)
+    ]
+)
 def test_parse(file_path, format, expected):
     assert parse(file_path, format) == expected
 
 
-@pytest.mark.parametrize("file_path1, file_path2, format,expected", [
-                        (JSON1, JSON2, 'stylish', STYLISH_BASIC),
-                        (YAML1, YAML2, 'stylish', STYLISH_BASIC),
-                        (NESTED_JSON1, NESTED_JSON2, 'stylish', STYLISH_NESTED),
-                        (NESTED_YAML1, NESTED_YAML2, 'stylish', STYLISH_NESTED),
-                        (NESTED_JSON1, NESTED_JSON2, 'plain', PLAIN_NESTED),
-                        (NESTED_YAML1, NESTED_YAML2, 'plain', PLAIN_NESTED),
-                        (NESTED_JSON1, NESTED_JSON2, 'json', JSON_NESTED),
-                        (NESTED_YAML1, NESTED_YAML2, 'json', JSON_NESTED)
-                        ])
+@pytest.mark.parametrize(
+    "file_path1, file_path2, format,expected", [
+        (JSON1, JSON2, 'stylish', STYLISH_BASIC),
+        (YAML1, YAML2, 'stylish', STYLISH_BASIC),
+        (NESTED_JSON1, NESTED_JSON2, 'stylish', STYLISH_NESTED),
+        (NESTED_YAML1, NESTED_YAML2, 'stylish', STYLISH_NESTED),
+        (NESTED_JSON1, NESTED_JSON2, 'plain', PLAIN_NESTED),
+        (NESTED_YAML1, NESTED_YAML2, 'plain', PLAIN_NESTED),
+        (NESTED_JSON1, NESTED_JSON2, 'json', JSON_NESTED),
+        (NESTED_YAML1, NESTED_YAML2, 'json', JSON_NESTED)
+    ]
+)
 def test_generate_diff(file_path1, file_path2, format, expected):
     assert generate_diff(file_path1, file_path2, format) == expected
